916f32195080933f24ca273593489ef29334792c
mahima sharma <sharmamahima0510@gmail.com>
2025-07-10 09:56:02 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
-
+console.log("✅ Loaded DB config:");
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? '****' : '(missing)');
+console.log("DB_NAME:", process.env.DB_NAME);
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
